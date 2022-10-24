003b6e7d765cb3b22db543b27e3bfc6499bdfe21
hester <hhhhhhesterd@outlook.com>
2022-10-24 00:30:53 +0000

    Solved cannot find cmpNode problem: Modified VFGNode2LLVMValue function

--- llvm/lib/Transforms/BlindedComputation/BlindedTaintTracking.cpp
+++ llvm/lib/Transforms/BlindedComputation/BlindedTaintTracking.cpp
@@ -16,6 +16,7 @@ void BlindedTaintTracking::buildTaintedSet(int iteration, Module& M) {
 	while (!vfgNodeWorkList.empty()) {
 		const SVF::VFGNode* vfgNode = vfgNodeWorkList.back();
 		vfgNodeWorkList.pop_back();
+		errs() << "handling: " << vfgNode->toString() << "\n";
 
 		if (handledNodes.count(vfgNode)) {
 			continue;
@@ -23,6 +24,9 @@ void BlindedTaintTracking::buildTaintedSet(int iteration, Module& M) {
 		handledNodes.insert(vfgNode);
 
 		const Value* valNode = VFGNode2LLVMValue(vfgNode);
+		if (valNode == nullptr) {
+			errs() << "ValNode is nullptr " << "\n"; 
+		}
 
 		if (vfgNode->getNodeKind() == SVF::VFGNode::VFGNodeK::Load) {
 			if (const LoadInst* LI = dyn_cast<LoadInst>(valNode)) {
@@ -55,13 +59,16 @@ void BlindedTaintTracking::buildTaintedSet(int iteration, Module& M) {
 					// TODO: handle ptr
 				}
 				else if (const CmpInst* CmpInstr = dyn_cast<CmpInst>(instr)) {
+					TaintedValues.insert(valNode);
+					LLVMContext &cont = instr->getContext();
+        	MDNode *N = MDNode::get(cont, ConstantAsMetadata::get(ConstantInt::get(cont, APInt(sizeof(long)*8, true, true))));
+        	const_cast<Instruction*>(instr)->setMetadata("my.md.blindedNTT", N);
 					for (auto CmpUser : valNode->users()) {
 						const Value* CmpUserVal = dyn_cast<Value>(CmpUser);
 						Value* NCmpUserVal = const_cast<Value*>(CmpUserVal);
 						const SVF::VFGNode* CmpVFGNode = LLVMValue2VFGNode(NCmpUserVal);
 						if (CmpVFGNode != nullptr) {
 							vfgNodeWorkList.push_back(CmpVFGNode);
-
 						}
 					}
 				}
@@ -80,40 +87,44 @@ void BlindedTaintTracking::buildTaintedSet(int iteration, Module& M) {
 
 			if (!handledNodes.count(dstNode)) {
 				vfgNodeWorkList.push_back(dstNode);
+				if (vfgNode->getNodeKind() == SVF::VFGNode::FParm) {
+					errs() << "VFGFParmNode: " << vfgNode->toString() << "\n" << dstNode->toString() << "\n";
+				}
 			}
+
 		}
 	}
 	printInstrsForConversion();
 }
 
 void BlindedTaintTracking::markInstrsForConversion() {
-	// for (auto Val : TaintedValues) {
-	// 	for (auto U : Val->users()) {
-	// 			auto ValUser = dyn_cast<Instruction>(U);
-	// 			if (const BranchInst *BrInst = dyn_cast<BranchInst>(&ValUser)) {
-	// 				if (BrInst->isConditional() && TaintedValues.count(BrInst->getCondition())){
-	// 					BranchInst* NBrInst = const_cast<BranchInst*>(BrInst);
-	// 					BlndBr.push_back(NBrInst);
-	// 				}
-	// 			}					
-	// 			else if (const LoadInst *LInst = dyn_cast<LoadInst>(ValUser)) {
-	// 				// Load using sensitive value
-	// 				// This is not handled yet. Current implementation actually handles GEP
-	// 				LoadInst* NLInst = const_cast<LoadInst*>(LInst);
-	// 				BlndMemOp.push_back(NLInst);
-	// 			}
-	// 			else if (const GetElementPtrInst *GEPInst = dyn_cast<GetElementPtrInst>(ValUser)) {
-	// 				GetElementPtrInst* NGEPInst = const_cast<GetElementPtrInst*>(GEPInst);
-	// 				BlndGep.push_back(NGEPInst);
-	// 			}
-	// 			else if (const StoreInst *SInst = dyn_cast<StoreInst>(ValUser)) {
-	// 				// Store using sensitive value
-	// 				// This is not handled yet. Current implementation actually handles GEP
-	// 				StoreInst* NSInst = const_cast<StoreInst*>(SInst);
-	// 				BlndMemOp.push_back(NSInst);
-	// 			}
-	// 	}
-	// }
+	for (auto Val : TaintedValues) {
+		for (auto U : Val->users()) {
+			auto ValUser = dyn_cast<Instruction>(U);
+			if (const BranchInst *BrInst = dyn_cast<BranchInst>(ValUser)) {
+				if (BrInst->isConditional() && TaintedValues.count(BrInst->getCondition())){
+					BranchInst* NBrInst = const_cast<BranchInst*>(BrInst);
+					BlndBr.push_back(NBrInst);
+				}
+			}					
+			else if (const LoadInst *LInst = dyn_cast<LoadInst>(ValUser)) {
+				// Load using sensitive value
+				// This is not handled yet. Current implementation actually handles GEP
+				LoadInst* NLInst = const_cast<LoadInst*>(LInst);
+				BlndMemOp.push_back(NLInst);
+			}
+			else if (const GetElementPtrInst *GEPInst = dyn_cast<GetElementPtrInst>(ValUser)) {
+				GetElementPtrInst* NGEPInst = const_cast<GetElementPtrInst*>(GEPInst);
+				BlndGep.push_back(NGEPInst);
+			}
+			else if (const StoreInst *SInst = dyn_cast<StoreInst>(ValUser)) {
+				// Store using sensitive value
+				// This is not handled yet. Current implementation actually handles GEP
+				StoreInst* NSInst = const_cast<StoreInst*>(SInst);
+				BlndMemOp.push_back(NSInst);
+			}
+		}
+	}
 
 }
 
@@ -167,34 +178,49 @@ const SVF::VFGNode* BlindedTaintTracking::LLVMValue2VFGNode(Value* value) {
 // Directly copied from SVF/SABER/ProgSlice.cpp
 // Just don't want to create object to use this method
 const Value* BlindedTaintTracking::VFGNode2LLVMValue(const SVF::SVFGNode* node) {
-   if(const SVF::StmtSVFGNode* stmt = SVF::SVFUtil::dyn_cast<SVF::StmtSVFGNode>(node))
-    {
-        if(SVF::SVFUtil::isa<SVF::StoreSVFGNode>(stmt) == false)
-        {
-            if(stmt->getPAGDstNode()->hasValue())
-                return stmt->getPAGDstNode()->getValue();
-        }
+   if(const SVF::StmtSVFGNode* stmt = SVF::SVFUtil::dyn_cast<SVF::StmtSVFGNode>(node)) {
+			if (SVF::SVFUtil::isa<SVF::StoreSVFGNode>(stmt)) {
+				return nullptr;
+			}            
+			else if (stmt->getPAGDstNode()->hasValue()) {
+				return stmt->getPAGDstNode()->getValue();
+			}
     }
     else if(const SVF::PHISVFGNode* phi = SVF::SVFUtil::dyn_cast<SVF::PHISVFGNode>(node))
     {
-        return phi->getRes()->getValue();
+			return phi->getRes()->getValue();
     }
     else if(const SVF::ActualParmSVFGNode* ap = SVF::SVFUtil::dyn_cast<SVF::ActualParmSVFGNode>(node))
     {
-        return ap->getParam()->getValue();
+			return ap->getParam()->getValue();
     }
     else if(const SVF::FormalParmSVFGNode* fp = SVF::SVFUtil::dyn_cast<SVF::FormalParmSVFGNode>(node))
     {
-        return fp->getParam()->getValue();
+			return fp->getParam()->getValue();
     }
     else if(const SVF::ActualRetSVFGNode* ar = SVF::SVFUtil::dyn_cast<SVF::ActualRetSVFGNode>(node))
     {
-        return ar->getRev()->getValue();
+			return ar->getRev()->getValue();
     }
     else if(const SVF::FormalRetSVFGNode* fr = SVF::SVFUtil::dyn_cast<SVF::FormalRetSVFGNode>(node))
     {
-        return fr->getRet()->getValue();
+			return fr->getRet()->getValue();
     }
+		else if (const SVF::CmpVFGNode* cmpVFGNode = dyn_cast<SVF::CmpVFGNode>(node)) {
+			return cmpVFGNode->getRes()->getValue();
+		}
+		else if (const SVF::BinaryOPVFGNode* binOpVFGNode = dyn_cast<SVF::BinaryOPVFGNode>(node)) {
+			return binOpVFGNode->getRes()->getValue();
+		}
+		else if (const SVF::UnaryOPVFGNode* unaryOpVFGNode = dyn_cast<SVF::UnaryOPVFGNode>(node)) {
+			const Value* unaryOpVal = unaryOpVFGNode->getRes()->getValue();
+			if (isa<UnaryOperator>(unaryOpVal)) {
+				return unaryOpVal;
+			}
+			else {
+				return nullptr;
+			}
+		}
 
     return nullptr;
 }
