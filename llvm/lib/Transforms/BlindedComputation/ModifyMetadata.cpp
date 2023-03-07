2d4fe5080e06174ad6cbfae1fe946749adff017f
hester <hhhhhhesterd@outlook.com>
2023-03-07 14:51:07 +0000

    modified metadata-modify pass

--- llvm/lib/Transforms/BlindedComputation/ModifyMetadata.cpp
+++ llvm/lib/Transforms/BlindedComputation/ModifyMetadata.cpp
@@ -3,32 +3,25 @@
 using namespace llvm;
 
 PreservedAnalyses ModifyMetadataPass::run(Module &M, ModuleAnalysisManager &AM) {
-    auto &TTResult = AM.getResult<BlindedTaintTracking>(M);
-    for (Function &F : M) {
+  auto &TTResult = AM.getResult<BlindedTaintTracking>(M);
+
+  for (Function &F : M) {
     if (F.isDeclaration()) {
       continue;
     }
 
-    // for (auto Arg = F.arg_begin(); Arg < F.arg_end(); ++Arg) {
-    //   if (Arg->hasAttribute(Attribute::Blinded)) {
-    //       Arg->removeAttr(Attribute::Blinded);
-    //   }
-    // }
-    for (auto I : TTResult.BlndBr) {
-      if (const BranchInst* BrInst = dyn_cast<BranchInst>(I)) {
-        BranchInst* NBrInst = const_cast<BranchInst*>(BrInst);
-        LLVMContext &cont = NBrInst->getContext();
-        MDNode *N = MDNode::get(cont, ConstantAsMetadata::get(ConstantInt::get(cont, APInt(sizeof(long)*8, true, true))));
-        NBrInst->setMetadata("t", N);
+    for (auto Arg = F.arg_begin(); Arg < F.arg_end(); ++Arg) {
+      if (Arg->hasAttribute(Attribute::Blinded)) {
+          Arg->removeAttr(Attribute::Blinded);
       }
     }
-    for (auto I : TTResult.BlndMemOp) {
-      if (const Instruction* MemOpInstr = dyn_cast<Instruction>(I)) {
-        Instruction* NMemOpInstr = const_cast<Instruction*>(MemOpInstr);
-        LLVMContext &cont = NMemOpInstr->getContext();
-        MDNode *N = MDNode::get(cont, ConstantAsMetadata::get(ConstantInt::get(cont, APInt(sizeof(long)*8, true, true))));
-        NMemOpInstr->setMetadata("t", N);
-      }
+  }
+  for (auto I : TTResult.BlndBr) {
+    if (const BranchInst* BrInst = dyn_cast<BranchInst>(I)) {
+      BranchInst* NBrInst = const_cast<BranchInst*>(BrInst);
+      LLVMContext &cont = NBrInst->getContext();
+      MDNode *N = MDNode::get(cont, ConstantAsMetadata::get(ConstantInt::get(cont, APInt(sizeof(long)*8, true, true))));
+      NBrInst->setMetadata("t", N);
     }
   }
   PreservedAnalyses PA = PreservedAnalyses::all();
