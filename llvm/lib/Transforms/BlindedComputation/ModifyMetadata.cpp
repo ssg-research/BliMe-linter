d7c2a513f0b0b379da38de33db881555dd5c8fd9
hester <hhhhhhesterd@outlook.com>
2023-03-28 14:06:13 +0000

    add metadata also for non-tainted instrs

--- llvm/lib/Transforms/BlindedComputation/ModifyMetadata.cpp
+++ llvm/lib/Transforms/BlindedComputation/ModifyMetadata.cpp
@@ -5,6 +5,14 @@ using namespace llvm;
 PreservedAnalyses ModifyMetadataPass::run(Module &M, ModuleAnalysisManager &AM) {
   auto &TTResult = AM.getResult<BlindedTaintTracking>(M);
 
+  for (auto I : TTResult.BlndBr) {
+    if (const BranchInst* BrInst = dyn_cast<BranchInst>(I)) {
+      BranchInst* NBrInst = const_cast<BranchInst*>(BrInst);
+      LLVMContext &cont = NBrInst->getContext();
+      MDNode *N = MDNode::get(cont, ConstantAsMetadata::get(ConstantInt::get(cont, APInt(sizeof(long)*8, true, true))));
+      NBrInst->setMetadata("t", N);
+    }
+  }
   for (Function &F : M) {
     if (F.isDeclaration()) {
       continue;
@@ -15,15 +23,19 @@ PreservedAnalyses ModifyMetadataPass::run(Module &M, ModuleAnalysisManager &AM) 
           Arg->removeAttr(Attribute::Blinded);
       }
     }
-  }
-  for (auto I : TTResult.BlndBr) {
-    if (const BranchInst* BrInst = dyn_cast<BranchInst>(I)) {
-      BranchInst* NBrInst = const_cast<BranchInst*>(BrInst);
-      LLVMContext &cont = NBrInst->getContext();
-      MDNode *N = MDNode::get(cont, ConstantAsMetadata::get(ConstantInt::get(cont, APInt(sizeof(long)*8, true, true))));
-      NBrInst->setMetadata("t", N);
+
+    for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; I++) {
+        auto N = I->getMetadata("t");
+        LLVMContext &cont = I->getContext();
+        if (N == NULL) {
+          N = MDNode::get(cont, ConstantAsMetadata::get(ConstantInt::get(cont, APInt(sizeof(long)*8, false, true))));
+          I->setMetadata("t", N);
+        }
     }
   }
+
+
+
   PreservedAnalyses PA = PreservedAnalyses::all();
   return PA;
 
